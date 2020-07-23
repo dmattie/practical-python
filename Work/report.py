eb46a4474e90b3ef7ee820e8dc339d5bc561efa6
dmattie <dmattie@stfx.ca>
2020-07-23 00:20:49 -0300

    E3.12

--- Work/report.py
+++ Work/report.py
@@ -5,34 +5,14 @@
 
 
 import csv
+import fileparse
 
 def read_portfolio(filename):
-    '''Computes the total cost (shares*price) of a portfolio file'''
-    portfolio = []
-    with open(filename, 'rt') as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        for row in rows:
-            holding={}
+    return fileparse.parse_csv(filename, select=['name','shares','price'], types=[str,int,float])
 
-            holding['name']=row[0]
-            holding['shares']=int(row[1])
-            holding['price']=float(row[2])
-            #holding = [row[0],int(row[1]),float(row[2])]
-            portfolio.append(holding)
-    return portfolio
 def read_prices(filename):
 
-    prices = {}
-    with open(filename) as f:
-        rows = csv.reader(f)
-        for row in rows:
-            try:
-                prices[row[0]] = float(row[1])
-            except IndexError:
-                pass
-
-    return prices
+    return dict(fileparse.parse_csv(filename,types=[str,float], has_headers=False))
 
 def make_report(portfolio, prices):
 
