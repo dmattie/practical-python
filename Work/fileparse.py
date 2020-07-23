a3591cd5325a1b15853c486cc4f90229f9abe83e
dmattie <dmattie@stfx.ca>
2020-07-23 00:10:12 -0300

    E3.7

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,33 +1,39 @@
 # fileparse.py
 import csv
 
-def parse_csv(filename,select=None,types=None,has_headers=True):
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
     '''
-    Parse a CSV file into a list of records
+    Parse a CSV file into a list of records with type conversion.
     '''
     with open(filename) as f:
-        rows = csv.reader(f)
+        rows = csv.reader(f, delimiter=delimiter)
 
-
+        # Read the file headers (if any)
         headers = next(rows) if has_headers else []
+
+        # If specific columns have been selected, make indices for filtering 
         if select:
-            indices = [headers.index(colname) for colname in select]
+            indices = [ headers.index(colname) for colname in select ]
             headers = select
-        else:
-            indices = []
 
         records = []
         for row in rows:
-            if not row:    # Skip rows with no data
+            if not row:     # Skip rows with no data
                 continue
-            # Filter the row if specific columns were selected
-            if indices:
-                row = [ row[index] for index in indices ]
+
+            # If specific column indices are selected, pick them out
+            if select:
+                row = [ row[index] for index in indices]
+
+            # Apply type conversion to the row
             if types:
-                row = [func(val) for func, val in zip(types, row) ]
-                
-            # Make a dictionary
-            record = dict(zip(headers, row))
+                row = [func(val) for func, val in zip(types, row)]
+
+            # Make a dictionary or a tuple
+            if headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
             records.append(record)
 
-    return records
+        return records
